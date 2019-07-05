68fca95f4aac621e2a3cb65f9fc387688b9835fd
Kelly Zick <kllyzck@gmail.com>
2019-07-05 13:46:39 -0600

    Change db name to palette_picker

--- knexfile.js
+++ knexfile.js
@@ -2,12 +2,23 @@
 module.exports = {
   development: {
     client: 'pg',
-    connection: 'postgres://localhost/colorsdb',
+    connection: 'postgres://localhost/palette_picker',
     migrations: {
       directory: './db/migrations'
     },
     seeds: {
       directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/palette_picker_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
     },
     useNullAsDefault: true
   }
